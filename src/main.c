7c0171ade7acdc2337451202fad68c4a0876cdc0
Tyler Hardison <tmhardis@ncsu.edu>
2025-05-15 14:55:50 -0400

    Attempt to read basic input

--- src/main.c
+++ src/main.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-  // Flush after every printf
-  setbuf(stdout, NULL);
-
-  //Prompt for input
-  printf("$ ");
-  
-
-  // Wait for user input
-  char input[100];
-  //When Using fgets there is a newline character at the end when reading
-  //Will Need to remove so that it can print properly
-  fgets(input, 100, stdin);
-  // Remove the trailing newline
-  input[strlen(input) - 1] = '\0';
+int main(int argc, char *argv[]) {  
+    // Flush after every printf
+    setbuf(stdout, NULL);
 
 
-  //TODO
-  //Prints for when the command is invalid for now, all commands are invalid
-  printf("%s: command not found", input);
-  return 0;
+
+    while(true) {
+        //Prompt for input
+        printf("$ ");
+        
+
+        // Wait for user input
+        char input[100];
+        //When Using fgets there is a newline character at the end when reading
+        //Will Need to remove so that it can print properly
+        fgets(input, 100, stdin);
+        // Remove the trailing newline
+        input[strlen(input) - 1] = '\0';
+        //Prints for when the command is invalid for now, all commands are invalid
+        printf("%s: command not found", input);
+    }
+
+    
+    return 0;
 }
