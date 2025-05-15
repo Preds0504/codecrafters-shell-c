390702abcad64592ab33af2246bd2fd6c4654aea
Tyler Hardison <tmhardis@ncsu.edu>
2025-05-15 14:21:54 -0400

    Attempt to read basic input

--- src/main.c
+++ src/main.c
@@ -2,10 +2,19 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+    char * input;
+    int buffer = 100;
+
+
   // Flush after every printf
   setbuf(stdout, NULL);
 
+  //This will print the $
+  //This is the prompt for the new command
   printf("$ ");
+  //Takes input from the command line
+  fgets(input, buffer, stdin);
+  printf("%s: command not found\n", input);
 
   // Wait for user input
   char input[100];
