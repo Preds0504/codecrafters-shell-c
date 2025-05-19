ce365d64f30169aa5669f8c709eccc7be3dc2bf9
Tyler Hardison <tmhardis@ncsu.edu>
2025-05-19 16:36:43 -0400

    Adding echo

--- src/main.c
+++ src/main.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]) {
         if (strcmp(input, "exit 0") == 0 ) {
             exit(0);
         } else if (strncmp(input, "echo", 4) == 0 ) {
-            printf("%s\n", input + 5);
+            // printf("%s\n", input + 5);
         } else {
             //Last case is the command doesn't exist
             printf("%s: command not found", input);
