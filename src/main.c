6fde96af8ba2275f918cdbc705e2d8c5bc91272d
Tyler Hardison <tylerhardison110@gmail.com>
2025-05-21 14:15:35 -0400

    run tests

--- src/main.c
+++ src/main.c
@@ -43,6 +43,7 @@ int handle_type (const char *command) {
         snprintf(full, sizeof(full), "%s/%s", dir, command);
         if (access(full, X_OK) == 0) {
             printf("%s is %s\n", command, full);
+            break;
         }
     } 
     return 0;
